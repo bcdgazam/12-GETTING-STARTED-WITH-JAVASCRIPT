c6d72b5eaec01a0c53f39da835bf2c36d42d5373
bcdazam <bcdazam@gmail.com>
2024-03-27 03:43:19 +0600

    string, number, boolean

--- 12-4.js
+++ 12-4.js
@@ -1,13 +1,15 @@
 //12-4 Variable type, Numeric, String, Boolean
 
-var seenAfter = 21;
-console.log(typeof seenAfter);
-
+//string
 var name = "Jogir Uddin";
 console.log(name);
 console.log(typeof name);
 
+//number
+var seenAfter = 21;
+console.log(typeof seenAfter);
 
+//boolean
 var isHot = false;
 var isRich = true;
 console.log(isHot);
