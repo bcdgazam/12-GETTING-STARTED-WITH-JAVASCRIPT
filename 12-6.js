b62980baed11ddbc6fd5f7eb52efdbd3e15b9695
bcdazam <bcdazam@gmail.com>
2024-03-28 14:34:11 +0600

    string, case, index and split

---
+++ 12-6.js
@@ -0,0 +1,8 @@
+//12-6 Explore string, case change, index, split
+
+
+var promise = "I love TO programming also I love to eat";
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf('TO'));
+console.log(promise.split('I'));
