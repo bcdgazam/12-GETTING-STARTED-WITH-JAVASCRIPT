6159a4b28202b4d650557e23a4222c3453ed9877
bcdazam <bcdazam@gmail.com>
2024-03-29 20:05:08 +0600

    Mathmetical Operators

---
+++ 12-8.js
@@ -0,0 +1,40 @@
+//12-8 Mathematical operations in JavaScript
+
+// =:Assignment Operator
+// +: Addition
+// -: Subtraction
+// *: Multiplication
+// /: Division
+// %: Modulus (remainder after division)
+// **: Exponentiation
+
+// +: Addition
+var priceOne = 25;
+var priceTwo = 10;
+var total = priceOne + priceTwo;
+console.log(total);
+
+// -: Subtraction
+var totalSub = priceOne - priceTwo;
+console.log(totalSub);
+
+// *: Multiplication
+var totalMultip = priceOne * priceTwo;
+console.log(totalMultip);
+
+// /: Division
+var totalDivi = priceOne / priceTwo;
+console.log(totalDivi);
+
+// %: Modulus (remainder after division)
+var totalModu = priceOne % priceTwo;
+console.log(totalModu);
+
+
+// ++: Increment 
+priceOne++;
+console.log(priceOne);
+
+// --: Decrement 
+priceTwo--;
+console.log(priceTwo);
