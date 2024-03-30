36a12c74d262cb583aed056bcee03109a90c006a
bcdazam <bcdazam@gmail.com>
2024-03-30 19:35:44 +0600

    if else

---
+++ 12-10.js
@@ -0,0 +1,42 @@
+//12-10 Make conditional decision, if-else, comparison
+
+var biscuitPrice = 22;
+
+if(biscuitPrice < 20){
+    console.log('I will eat the biscuits');
+}
+else{
+    console.log('Just Tea is Better :)')
+}
+
+
+
+var SobjiPrice = 22;
+
+if(SobjiPrice > 20){
+    console.log('I will eat the Sobji');
+}
+else{
+    console.log('Just dal is Better')
+}
+
+
+
+var goPicnic = 20;
+
+if(goPicnic == 20){
+    console.log('Wi will go to picnic');
+}
+else{
+    console.log('Sorry, We want to stay home due to not fill up money')
+}
+
+
+var goPicnic = 10;
+
+if(goPicnic != 10){
+    console.log('Wi will go to picnic');
+}
+else{
+    console.log('Sorry, We want to stay home due to not fill up money')
+}
