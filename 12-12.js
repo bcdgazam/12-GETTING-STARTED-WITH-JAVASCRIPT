29695c5ba5d87e0c890003a4931a12f6ab4e4596
Golam Azam <bcdazam@gmail.com>
2024-04-22 12:47:50 +0600

    case change

--- 12-12.js
+++ 12-12.js
@@ -1,14 +1,13 @@
 //12-12 JavaScript Date timezone and Module Summary
 
-
 /*
     12-1 Module overview, need for Javascript
     12-2 Run JavaScript in browser, VSCode, and codepen
     12-3 What is variable, declare a variable, see output
     12-4 Variable type, Numeric, String, Boolean
     12-5 Variable name naming convention and best practice
-    12-6 Explore string case change insex split
-    12-7 Integer float parseInt parseFloat type conversion
+    12-6 Explore string case change index split
+    // 12-7 Integer float parseInt parseFloat type conversion
     12-8 Mathematical operations in JavaScript
     12-9 Math absolute round floor ceil random
     12-10 Make conditional decision, if-else, comparison
@@ -18,3 +17,10 @@
 
 //Exercise
 
+let stCase = 'Hello bangladesh';
+console.log(stCase.toUpperCase());
+console.log(stCase.toLocaleLowerCase());
+console.log(stCase.indexOf('b'))
+console.log(stCase.split());
+
+    // 12-7 Integer float parseInt parseFloat type conversion
