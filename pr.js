ad8ce92352e1ec8b260d54a33244a2dd39c6e5c8
Golam Azam <bcdazam@gmail.com>
2024-05-04 09:46:28 +0600

    practice

---
+++ pr.js
@@ -0,0 +1,12 @@
+let strEx = 'Hello World';
+console.log(strEx);
+
+
+let num = 23;;
+let num2 = '23.34';
+num2 = Math.abs(num2);
+console.log(num + num2);
+let result = Math.random(num + num2) * 100;
+result = result.toFixed(2);
+result = Math.abs(result);
+console.log(result);
