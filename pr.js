416b5ecb5c82894787650076b63d9847e13188bc
Golam Azam <bcdazam@gmail.com>
2024-05-04 10:28:53 +0600

    practice

--- pr.js
+++ pr.js
@@ -1,12 +1,40 @@
-let strEx = 'Hello World';
-console.log(strEx);
+// let strEx = 'Hello World';
+// console.log(strEx);
 
 
-let num = 23;;
-let num2 = '23.34';
-num2 = Math.abs(num2);
-console.log(num + num2);
-let result = Math.random(num + num2) * 100;
-result = result.toFixed(2);
-result = Math.abs(result);
-console.log(result);
+// let num = 23;;
+// let num2 = '23.34';
+// num2 = Math.abs(num2);
+// console.log(num + num2);
+// let result = Math.random(num + num2) * 100;
+// result = result.toFixed(2);
+// result = Math.abs(result);
+// console.log(result);
+
+// let isHot = true;
+// let isCold = false;
+
+// let speech = 'Hello Bangladesh, i am a cow, But want to learn programming, I think my brain will hel me. All the best'
+// console.log(speech.indexOf(' i '));
+
+// let banaPrice = 19;
+
+// if(banaPrice < 20){
+//     console.log('Banana Khabo')
+// }
+// else{
+//     console.log('Tor banana Khaoya lagbe na, tui beta gorib')
+// }
+
+let getJob = false;
+let savedAmount = 500000;
+
+if(getJob == true && savedAmount > 500000){
+    console.log('Tomar risk neya uchit')
+}
+else if(getJob == true){
+    console.log('Wait for the time to come')
+}
+else{
+    console.log('Dhoirjo dhora mohot gun, Thanks!')
+}
