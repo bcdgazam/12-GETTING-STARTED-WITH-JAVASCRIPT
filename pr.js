13528c4871545b7d4192aab8daaa8f32f7574c73
Golam Azam <bcdazam@gmail.com>
2024-05-05 09:15:58 +0600

    math practice

--- pr.js
+++ pr.js
@@ -1,2 +1,16 @@
-let speech = 'I love bangladesh!';
-console.log(speech);
+// let speech = 'I love bangladesh!';
+// let newSPeech = speech.toUpperCase();
+// let newSPeech2 = speech.toLowerCase();
+// let splitSpeech = speech.split("love")
+// console.log(splitSpeech);
+
+
+let num1 = -20;
+let num2 = 25.36;
+
+num1 = Math.abs(num1);
+console.log(num1);
+
+num2 = Math.random(num2)
+console.log(num1 + num2);
+
