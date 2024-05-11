5631898563ff8782dcb9e161cd98c8280725e02f
Golam Azam <bcdazam@gmail.com>
2024-05-11 19:53:27 +0600

    practice

--- pr.js
+++ pr.js
@@ -1,27 +1,2 @@
-// let speech = 'I love bangladesh!';
-// let newSPeech = speech.toUpperCase();
-// let newSPeech2 = speech.toLowerCase();
-// let splitSpeech = speech.split("love")
-// console.log(splitSpeech);
-
-
-// let num1 = -20;
-// let num2 = 25.36;
-
-// num1 = Math.abs(num1);
-// console.log(num1);
-
-// num2 = Math.random(num2)
-// console.log(num1 + num2);
-
-// let bananaPrice = 24;
-
-// if(bananaPrice < 22){
-//     console.log('Banana Khabo')
-// }
-// else{
-//     console.log('Kaje mon de, Taka nai, Khaoyao nai.')
-// }
-
-let newDate = new Date();
-console.log(newDate);
+let speech = 'Hi, am golam, Trying to understand the javascript';
+console.log(speech);
