e7da27990216fd58cd147f162c14720d85008c46
Golam Azam <bcdazam@gmail.com>
2024-05-11 20:03:08 +0600

    type conversion

--- pr.js
+++ pr.js
@@ -1,2 +1,10 @@
-let speech = 'Hi, am golam, Trying to understand the javascript';
-console.log(speech);
+// let speech = 'Hi, am golam, Trying to understand the javascript';
+// console.log(speech.split('to'));
+
+let num1 = 20;
+let num2 = '30.23';
+
+// num2 = parseFloat(num2);
+num2 = parseInt(num2);
+console.log(num1 + num2);
+
