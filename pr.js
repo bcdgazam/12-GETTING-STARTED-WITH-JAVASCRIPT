5ed5e48483c667b14bf901423e4d8efa9852b6a7
Golam Azam <bcdazam@gmail.com>
2024-05-14 22:57:12 +0600

    practice

--- pr.js
+++ pr.js
@@ -1,10 +1,2 @@
-// let speech = 'Hi, am golam, Trying to understand the javascript';
-// console.log(speech.split('to'));
-
-let num1 = 20;
-let num2 = '30.23';
-
-// num2 = parseFloat(num2);
-num2 = parseInt(num2);
-console.log(num1 + num2);
-
+let speech = 'I love bangladesh';
+console.log(speech);
