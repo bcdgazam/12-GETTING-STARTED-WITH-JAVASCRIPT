a0d7e80e50fabb77063908be9a4160060db0e2ab
Golam Azam <bcdazam@gmail.com>
2024-05-15 22:58:35 +0600

    practice

--- pr.js
+++ pr.js
@@ -1,2 +1,17 @@
-let speech = 'I love bangladesh';
-console.log(speech);
+// let speech = 'I love bangladesh';
+// console.log(speech.toUpperCase());
+// console.log(speech.toLocaleLowerCase());
+// console.log(speech.split('love'));
+// console.log(speech.indexOf('e'));
+
+// let num1 = 23;
+// let num2 = '20.34';
+
+// let result = parseFloat(num2);
+// console.log(result);
+
+// let result2 = num1.toString();
+// console.log(result2);
+
+let num1 = -90;
+console.log(Math.abs(num1));
