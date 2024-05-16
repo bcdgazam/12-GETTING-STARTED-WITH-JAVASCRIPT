f32f6a98655bcc442b8eea24cbf1221e192bee45
Golam Azam <bcdazam@gmail.com>
2024-05-16 10:35:35 +0600

    if else practice

--- pr.js
+++ pr.js
@@ -13,10 +13,21 @@
 // let result2 = num1.toString();
 // console.log(result2);
 
-let num1 = -90;
-num1 = Math.abs(num1);
-let num2 = '30.70';
-num2 = parseFloat(num2);
-let result = num1 + num2;
-result = Math.random(result) * 100;
-console.log(result);
+// let num1 = -90;
+// num1 = Math.abs(num1);
+// let num2 = '30.70';
+// num2 = parseFloat(num2);
+// let result = num1 + num2;
+// result = Math.random(result) * 100;
+// let newResult = result.toFixed(2);
+// newResult = Math.abs(newResult);
+// console.log(newResult);
+
+let bananaPrice = 22;
+
+if(bananaPrice < 20){
+    console.log('I will eat the banana')
+}
+else{
+    console.log('Sorry I have to eat air');
+}
